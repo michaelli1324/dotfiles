19cc2fc61cac6df80a0dcecf4abf3b27ae69dcea
Michael Li <michael.li1324@gmail.com>
2026-02-11 22:54:40 -0800

    add nvim configs

---
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,10 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  opts = {
+    current_line_blame = true,
+    current_line_blame_opts = {
+      delay = 200,
+      virt_text_pos = "eol",
+    },
+  },
+}
